a89145ef03d56f16866e2a3e70cd655f106ea1a7
BuildTools <mreilaender@student.tgm.ac.at>
2016-02-15 22:32:32 +0100

    added None typ and wrong file extension validation

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 
 class Model(object):
@@ -10,6 +11,10 @@ class Model(object):
         pass
 
     def read_csv(self, filename, delimiter=' ', quotechar='|'):
+        if filename is None:
+            raise TypeError("1st parameter was from type None")
+        if not filename.endswith('.csv'):
+            raise IOError("Wrong filetype, expected file extension was %s, got %s instead" % ('.csv', os.path.splitext(filename)[1]))
         out = ""
         with open(filename, newline='\n') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
