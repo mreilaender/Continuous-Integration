d5db00a508fbc3d7ed8de11de6466a12c7a70c33
BuildTools <mreilaender@student.tgm.ac.at>
2016-02-16 13:40:31 +0100

    added test cases none obj for read_csv and wrong file extension

---
+++ src/test/test_model.py
@@ -0,0 +1,16 @@
+import unittest
+from src.model import Model
+
+
+class TestModel(unittest.TestCase):
+    def setUp(self):
+        self.model = Model()
+
+    def tearDown(self):
+        del self.model
+
+    def test_read_csv_none_obj(self):
+        self.assertRaises(TypeError, self.model.read_csv, None)
+
+    def test_wrong_filetype_no_csv(self):
+        self.assertRaises(IOError, self.model.read_csv, "..\\resources\\gui.ui")
