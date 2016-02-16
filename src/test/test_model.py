fd7d88a13841a1cd09e692bb0281c0ae82994af5
BuildTools <mreilaender@student.tgm.ac.at>
2016-02-16 14:03:08 +0100

    added resource path, now referencing on it

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -1,10 +1,12 @@
 import unittest
+import os
 from src.model import Model
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
         self.model = Model()
+        self.resource_path = os.path.abspath('../../resources')
 
     def tearDown(self):
         del self.model
@@ -13,4 +15,4 @@ class TestModel(unittest.TestCase):
         self.assertRaises(TypeError, self.model.read_csv, None)
 
     def test_wrong_filetype_no_csv(self):
-        self.assertRaises(IOError, self.model.read_csv, "..\\resources\\gui.ui")
+        self.assertRaises(IOError, self.model.read_csv, self.resource_path + '/gui.ui')
